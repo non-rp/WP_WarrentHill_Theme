fcfdbf4b038e9eff6f1bdaf5e3d016331cca1660
Valentyn Melnychenko <68907352+non-rp@users.noreply.github.com>
2026-01-08 17:55:43 +0200

    Clear database configuration in wp-config.php
    Removed database credentials from wp-config.php.

--- wp-config.php
+++ wp-config.php
@@ -23,16 +23,16 @@ define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'plastikb_will' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'plastikb_will' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'h^3aFd23H&' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'plastikb.mysql.tools' );
+define( 'DB_HOST', '' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
